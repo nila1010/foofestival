d14f49fd8b098699003a054b4fe95c6c3c108c37
NL <nila0001@stud.kea.dk>
2024-05-27 14:36:43 +0200

    fisk

--- src/app/artists/page.jsx
+++ src/app/artists/page.jsx
@@ -3,19 +3,9 @@ import { getData, getLikes } from "@/lib/crud";
 import Inner from "./Inner";
 
 export default async function Page() {
-  const { data, likes } = await fetchData();
+  const data = await getData("bands");
 
-  async function fetchData() {
-    /* await new Promise((resolve) => {
-      setTimeout(() => {
-        resolve("Operation successful");
-      }, 2000);
-    }); */
-    const data = await getData("bands");
-
-    const likes = await getLikes("Nicolai");
-    return { data, likes };
-  }
+  const likes = await getLikes("Nicolai");
 
   return (
     <Inner
