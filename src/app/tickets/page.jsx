e8ca0476737a8e17b8dfa833a9b0973041774af3
NL <nila0001@stud.kea.dk>
2024-05-27 15:55:45 +0200

    v1

--- src/app/tickets/page.jsx
+++ src/app/tickets/page.jsx
@@ -20,7 +20,7 @@ export default function Ticket() {
   const [availableSpots, setAvailableSpots] = useState();
   const [resId, setResId] = useState();
   const [info, setInfo] = useState();
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(5);
   const [greenOpt, setGreenOpt] = useState(false);
   const [tents, setTents] = useState();
   const [bookingInfo, setBookingInfo] = useState({});
