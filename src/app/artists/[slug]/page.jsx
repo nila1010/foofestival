3d7d3df75289b32b75dd7e5fda219dd907ff7757
NL <nila0001@stud.kea.dk>
2024-05-30 21:37:39 +0200

    fix

--- src/app/artists/[slug]/page.jsx
+++ src/app/artists/[slug]/page.jsx
@@ -2,6 +2,24 @@ import Inner from "./Inner";
 import combinedData from "@/lib/combineData";
 import { getData } from "@/lib/crud";
 
+export async function generateStaticParams() {
+  const data = await getData("bands");
+  return data.map((oneBand) => ({
+    slug: oneBand.slug,
+  }));
+}
+
+export async function generateMetadata({ params }) {
+  const { slug } = params;
+  const fetchData = await getData("bands");
+  const filterData = fetchData.filter((oneBand) => oneBand.slug === slug);
+  const data = filterData[0];
+  if (!data) return NotFound();
+  return {
+    title: `${data.name} - FooFestival, where the vibrant energy of youth converges with the electrifying beats"`,
+  };
+}
+
 export default async function Page({ params }) {
   const { slug } = params;
   const fetchData = await combinedData();
